b3218a872df0dab669f961600d0c655c0e7f3a61
felos <felos@ciencias.unam.mx>
2021-12-05 19:59:56 -0600

    Correcion menor

--- lib/libNSRDB.py
+++ lib/libNSRDB.py
@@ -1,6 +1,6 @@
 # Configuración de usuario.
-API_KEY = "gfYiPMGMLLOMK6h8PcSp2102kPZrFFwFIqp5vSM9"
-EMAIL   = "felos@ciencias.unam.mx"
+API_KEY = None
+EMAIL   = None
 # -------------------------------------------
 
 
